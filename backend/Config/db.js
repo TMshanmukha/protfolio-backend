98d160d19ad4d5028b550053b8a2ca164a1f5041
TM SHANMUKHA CHANDRRA <shanmu547@gmail.com>
2026-07-31 20:52:09 +0530

    issue 1

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -4,6 +4,8 @@ dotenv.config();
 
 const db = async () => {
     try{
+        console.log("MONGO_URL exists:", !!process.env.MONGO_URL);
+        console.log("MONGO_URL:", process.env.MONGO_URL?.substring(0, 25));
         await connect(process.env.MONGO_URL)
 
         console.log("MongoDB connected");
